173e2c414811048ccae17e9658ca41fb4baaeb79
BT_1N3_28 <jjacobs8462@bths.edu>
2019-04-01 08:32:14 -0400

    finished the activity

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,21 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int firstInt;
+    private int initialSecondInt;
+
+    public MultPractice(int firstInt, int initialSecondInt)
+    {
+        this.firstInt = firstInt;
+        this.initialSecondInt = initialSecondInt;
+    }
+
+    public String getProblem() {
+        return this.firstInt + " TIMES " + this.initialSecondInt;
+    }
+
+    public void nextProblem() {
+        this.initialSecondInt++;
+    }
 }
